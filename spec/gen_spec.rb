dfbe99f42fb07c8b2bd9e1d50551d759747d921f
Tymon Tobolski <i@teamon.eu>
2013-06-07 01:01:48 +0200

    Refactored specs + options specs

--- spec/gen_spec.rb
+++ spec/gen_spec.rb
@@ -18,19 +18,37 @@ describe "Gen" do
   end
 
   it "should handle :nolog option" do
-    cmds = addon.generate["start"]["commands"]
-    cmds.must_include "test-log 2>&1 >> SERVICE_PREFIX/service.log"
-    cmds.must_include "test-nolog"
-    cmds.wont_include "test-nolog 2>&1 >> SERVICE_PREFIX/service.log"
+    addon.generate["start"]["commands"].tap do |i|
+      i.must_include "test-log 2>&1 >> SERVICE_PREFIX/service.log"
+      i.must_include "test-nolog"
+      i.wont_include "test-nolog 2>&1 >> SERVICE_PREFIX/service.log"
+    end
   end
 
   it "should handle touch" do
-    cmds = addon.generate["validate"]["commands"]
-    cmds.must_include "touch SERVICE_PREFIX/test-file"
+    addon.generate["validate"]["commands"].tap do |i|
+      i.must_include "touch SERVICE_PREFIX/test-file"
+    end
   end
 
   it "should handle mkdir" do
-    cmds = addon.generate["validate"]["commands"]
-    cmds.must_include "test ! -d SERVICE_PREFIX/test-dir && mkdir -p SERVICE_PREFIX/test-dir"
+    addon.generate["validate"]["commands"].tap do |i|
+      i.must_include "test ! -d SERVICE_PREFIX/test-dir && mkdir -p SERVICE_PREFIX/test-dir"
+    end
+  end
+
+  it "should handle options" do
+    addon.generate["start"]["commands"].tap do |i|
+      i.must_include "test-opt1 x"
+      i.must_include "test-opt2 123"
+    end
+    addon.generate(:opt1 => "y")["start"]["commands"].tap do |i|
+      i.must_include "test-opt1 y"
+      i.must_include "test-opt2 123"
+    end
+    addon.generate(:opt1 => "z", :opt2 => 0)["start"]["commands"].tap do |i|
+      i.must_include "test-opt1 z"
+      i.must_include "test-opt2 0"
+    end
   end
 end
