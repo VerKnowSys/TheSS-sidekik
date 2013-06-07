be42fe06798fd473d63f98a6c81b98d1cec95d53
Tymon Tobolski <i@teamon.eu>
2013-06-07 02:22:27 +0200

    New add-on: Memcached

---
+++ lib/hussar/addons/memcached.rb
@@ -0,0 +1,12 @@
+addon "Memcached" do |a|
+  a.software_name "Memcached"
+
+  a.start do
+    sh %Q{
+      SERVICE_ROOT/exports/memcached \\
+        -l SERVICE_ADDRESS \\
+        -p SERVICE_PORT \\
+        -P SERVICE_PREFIX/service.pid -d
+    }
+  end
+end
